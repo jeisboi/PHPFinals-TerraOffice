88d09373126e7a35bf27c2974b457f30d02070ac
Jeici <154306218+jeisboi@users.noreply.github.com>
2026-07-18 19:35:49 +0800

    Delete PHPFinals_TerraOffice directory

--- PHPFinals_TerraOffice/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer>
-  <p><strong>Group Name:</strong> TerraOffice Team</p>
-  <p>Disclaimer: This website is for educational purposes only and is a requirement for our final project.</p>
-</footer>
-</body>
-</html>
